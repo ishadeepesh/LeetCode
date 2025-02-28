93e2443670349951b953c0d7964e2ba9ea91e5a8
ishadeepesh <115734449+ishadeepesh@users.noreply.github.com>
2025-03-01 00:19:10 +0530

    Update and rename 49. Group Anagrams to 49. Group Anagrams.py

--- 49. Group Anagrams.py
+++ 49. Group Anagrams.py
@@ -1,7 +1,7 @@
-Given an array of strings strs, group the anagrams together. You can return the answer in any order.
-Example 1:
-Input: strs = ["eat","tea","tan","ate","nat","bat"]
-Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+# Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+# Example 1:
+# Input: strs = ["eat","tea","tan","ate","nat","bat"]
+# Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
 
 
 from collections import defaultdict
