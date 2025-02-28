e78f17c7142b041161b5a9177267ac88e92b655f
ishadeepesh <115734449+ishadeepesh@users.noreply.github.com>
2025-03-01 00:18:36 +0530

    Update and rename 242. Valid Anagram to 242. Valid Anagram.py

--- 242. Valid Anagram.py
+++ 242. Valid Anagram.py
@@ -1,7 +1,7 @@
-Given two strings s and t, return true if t is an anagram of s, and false otherwise.
-Example 1:
-Input: s = "anagram", t = "nagaram"
-Output: true
+# Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+# Example 1:
+# Input: s = "anagram", t = "nagaram"
+# Output: true
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
