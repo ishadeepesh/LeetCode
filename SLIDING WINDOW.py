df247e8478afdaa294dcc211a574ae20c592f073
ishadeepesh <115734449+ishadeepesh@users.noreply.github.com>
2025-03-19 00:17:25 +0530

    Update SLIDING WINDOW.py

--- SLIDING WINDOW.py
+++ SLIDING WINDOW.py
@@ -1,4 +1,4 @@
-# 643. Maximum Average Subarray I
+643. Maximum Average Subarray I
 # You are given an integer array nums consisting of n elements, and an integer k.
 # Find a contiguous subarray whose length is equal to k that has the maximum average value and return this value. Any answer with a calculation error less than 10-5 will be accepted.
 # Example 1:
@@ -41,6 +41,29 @@ class Solution:
         #         l+=1
         #     r+=1
         # return maxval
+
+209. Minimum Size Subarray Sum
+# Given an array of positive integers nums and a positive integer target, return the minimal length of a subarray whose sum is greater than or equal to target. 
+# If there is no such subarray, return 0 instead.
+# Example 1:
+# Input: target = 7, nums = [2,3,1,2,4,3]
+# Output: 2
+# Explanation: The subarray [4,3] has the minimal length under the problem constraint.
+
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        windowSum=0
+        windowStart=0
+        minlength=float('inf')
+
+        for windowEnd in range(len(nums)):
+            windowSum+=nums[windowEnd]
+            while windowSum>=target:
+                minlength=min(minlength,windowEnd-windowStart+1)
+                windowSum-=nums[windowStart]
+                windowStart+=1
+
+        return minlength if minlength<float('inf') else 0
 
        
 
